ff9baa87ace6dd980cf61f903b3cf8f7fa9e4413
jamiewallace <wallacejamiec@gmail.com>
2014-01-06 11:38:21 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Btwebsite::Application.config.secret_token = '17b38cc51193c4969aa7a716615af1fc23cc257929f4c08a4167761b39a47dfc2d014dc726af195d0ecd7981492ac521c55e22587189559d52daec7f74194858'
